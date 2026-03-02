256d79a4a7d14aeb4db43906a12fde323da2b3df
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-02 12:00:42 +0100

    The board should only have one knight and pawn

--- src/test/java/it/unibo/pps/e2/LogicTest.java
+++ src/test/java/it/unibo/pps/e2/LogicTest.java
@@ -1,15 +1,51 @@
 package it.unibo.pps.e2;
 import org.junit.jupiter.api.*;
 
-public class LogicTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+public class LogicTest {
+    private Logics logics;
+    private static final int BOARD_SIZE = 5;
+
+    @BeforeEach
+    public void init() {
+        logics = new LogicsImpl(BOARD_SIZE);
+    }
+
+    @Test
+    public void test() {
+        assert (true);
+        // TODO: Add your test logic here
+        // You can generate random inputs and assert the expected output
+        // For example:
+        // int result = Logic.someMethod(5, 10);
+        // assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void chessboardShouldHaveOnlyOneKnight() {
+        int knights = 0;
+        for (int column = 0; column < BOARD_SIZE; column++) {
+            for (int row = 0; row < BOARD_SIZE; row++) {
+                if (logics.hasKnight(row, column)) {
+                    knights++;
+                }
+            }
+        }
+        assertEquals(1, knights);
+    }
+
+    @Test
+    public void chessboardShouldHaveOnlyOnePawn() {
+        int pawns = 0;
+        for (int column = 0; column < BOARD_SIZE; column++) {
+            for (int row = 0; row < BOARD_SIZE; row++) {
+                if (logics.hasPawn(row, column)) {
+                    pawns++;
+                }
+            }
+        }
+        assertEquals(1, pawns);
+    }
+
 }
