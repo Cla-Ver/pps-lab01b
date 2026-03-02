583ac61afdfc241ad1f992051a30eabc5eaa9574
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-02 09:58:39 +0100

    Split BankAccountTest in Core and Silver

---
+++ src/test/java/it/unibo/pps/e1/CoreBankAccountTest.java
@@ -0,0 +1,27 @@
+package it.unibo.pps.e1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CoreBankAccountTest {
+    protected static final int INITIAL_DEPOSIT = 1000;
+    protected BankAccount account;
+
+    @BeforeEach
+    void init() {
+        this.account = new SilverBankAccount();
+    }
+
+    @Test
+    public void testInitiallyEmpty() {
+        assertEquals(0, this.account.getBalance());
+    }
+
+    @Test
+    public void testCanDeposit() {
+        this.account.deposit(INITIAL_DEPOSIT);
+        assertEquals(INITIAL_DEPOSIT, this.account.getBalance());
+    }
+}
---
+++ src/test/java/it/unibo/pps/e1/SilverBankAccountTest.java
@@ -0,0 +1,25 @@
+package it.unibo.pps.e1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SilverBankAccountTest extends CoreBankAccountTest {
+
+    @Test
+    public void testCanWithdraw() {
+        final int withdrawAmount = 200;
+        this.account.deposit(INITIAL_DEPOSIT);
+        this.account.withdraw(withdrawAmount);
+        assertEquals(INITIAL_DEPOSIT - withdrawAmount - 1, this.account.getBalance());
+    }
+
+    @Test
+    public void testCannotWithdrawMoreThanAvailable(){
+        this.account.deposit(INITIAL_DEPOSIT);
+        assertThrows(IllegalStateException.class, () -> this.account.withdraw(1200));
+    }
+
+}
