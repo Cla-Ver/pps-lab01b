5aee15de1e8699bd01971ada2c1e6f7718d53ddd
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-02 09:58:20 +0100

    Extracted interface from CoreBankAccount

---
+++ src/main/java/it/unibo/pps/e1/SilverBankAccount.java
@@ -0,0 +1,24 @@
+package it.unibo.pps.e1;
+
+public class SilverBankAccount implements BankAccount {
+
+    private final CoreBankAccount base = new CoreBankAccount();
+
+    @Override
+    public int getBalance() {
+        return base.getBalance();
+    }
+
+    @Override
+    public void deposit(int amount) {
+        base.deposit(amount);
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if (this.getBalance() < amount){
+            throw new IllegalStateException();
+        }
+        base.withdraw(amount + 1);
+    }
+}
