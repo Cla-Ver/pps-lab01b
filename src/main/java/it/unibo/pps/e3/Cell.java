7fdd343afa74cf22744a0761e5abdf2239abe153
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-06 10:55:58 +0100

    Added documentation for hit

--- src/main/java/it/unibo/pps/e3/Cell.java
+++ src/main/java/it/unibo/pps/e3/Cell.java
@@ -1,5 +1,12 @@
 package it.unibo.pps.e3;
 
 public interface Cell {
+    /**
+     *
+     * Checks whether the cell has a mine or not.
+     *
+     * @return true if the cell is armed.
+     *
+     * */
     boolean hit();
 }
