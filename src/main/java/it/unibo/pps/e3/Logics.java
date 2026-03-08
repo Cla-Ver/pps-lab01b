497158226c7d1112d4139c25a40236dc1cba84b4
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-08 12:18:20 +0100

    Documented interface

--- src/main/java/it/unibo/pps/e3/Logics.java
+++ src/main/java/it/unibo/pps/e3/Logics.java
@@ -2,7 +2,22 @@ package it.unibo.pps.e3;
 
 public interface Logics {
 
+    /**
+     *
+     * Retrieves the number of cells of the minefield.
+     *
+     * @return The number of cells.
+     *
+     * */
     long getNumberOfCells();
 
+    /**
+     *
+     * How many mines there are on the minefield.
+     *
+     * @return The amount of mines.
+     *
+     * */
     long countMines();
+
 }
